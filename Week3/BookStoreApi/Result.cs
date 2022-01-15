1cf63797d9913250b3415a7642bf97939ec51005
Fatma Sena Ekiz <63646687+neptunel@users.noreply.github.com>
2022-01-14 23:45:42 -0500

    Add files via upload

---
+++ Week3/BookStoreApi/Result.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi
+{
+     public class Result  /*A Result class, response format*/
+    {
+        public int HttpStatusCode { get; set; }
+        public string Message  { get; set; }
+    }
+}
